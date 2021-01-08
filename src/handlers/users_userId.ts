8af923496c7b703d848a48e89f64b44528dc0b41
Munieru <20086673+munierujp@users.noreply.github.com>
2021-01-08 15:28:31 +0900

    use errorHandler

--- src/handlers/users_userId.ts
+++ src/handlers/users_userId.ts
@@ -1,9 +1,11 @@
 import { StatusCodes } from 'http-status-codes'
 import middy from '@middy/core'
 import { Controller } from '../controllers/users_userId'
-import { validator } from '../middlewares'
 import {
-  createErrorResponse,
+  errorHandler,
+  validator
+} from '../middlewares'
+import {
   importAws
 } from '../modules'
 import type {
@@ -20,15 +22,10 @@ export type Result = APIGatewayProxyResult
 
 export const handler = middyfy(async (event) => {
   const controller = new Controller({ aws })
-
-  try {
-    const result = await controller.execute(event)
-    return {
-      statusCode: StatusCodes.OK,
-      body: JSON.stringify(result)
-    }
-  } catch (e) {
-    return createErrorResponse(e)
+  const result = await controller.execute(event)
+  return {
+    statusCode: StatusCodes.OK,
+    body: JSON.stringify(result)
   }
 })
 
@@ -53,4 +50,5 @@ function middyfy (handler: Handler<Event, Result>): middy.Middy<Event, Result> {
         }
       }
     }))
+    .use(errorHandler())
 }
