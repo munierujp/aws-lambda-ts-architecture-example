8daaec5830d71e1f93a0be89e04867f2e5bd183f
Munieru <20086673+munierujp@users.noreply.github.com>
2021-01-10 00:09:13 +0900

    remove controller (#25)
    * remove controller
    
    * remove aws-lambda-mock-context package
    
    * fix package.json
    
    remove aws-lambda-mock-context from devDependencies

--- src/handlers/users_userId.ts
+++ src/handlers/users_userId.ts
@@ -1,23 +1,40 @@
+import { HTTPMethod } from 'http-method-enum'
+import { StatusCodes } from 'http-status-codes'
 import middy from '@middy/core'
-import { Controller } from '../controllers/users_userId'
+import { InvalidMethodError } from '../errors'
+import { UserRepository } from '../interfaces/dynamodb'
 import {
   errorHandler,
   validator
 } from '../middlewares'
 import { importAws } from '../modules'
-import type { Handler } from 'aws-lambda'
+import { userGetter } from '../usecases'
 import type {
-  Event,
-  Result
-} from '../controllers/users_userId'
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler
+} from 'aws-lambda'
 
 const aws = importAws({
   xray: '_X_AMZN_TRACE_ID' in process.env
 })
 
+export interface Event extends APIGatewayProxyEvent {
+  pathParameters: {
+    userId: string
+  }
+}
+export type Result = APIGatewayProxyResult
+
 export const handler = middyfy(async (event) => {
-  const controller = new Controller({ aws })
-  return await controller.execute(event)
+  const httpMethod = event.httpMethod.toUpperCase()
+
+  switch (httpMethod) {
+    case HTTPMethod.GET:
+      return await executeUserGetter(event)
+    default:
+      throw new InvalidMethodError(`invalid method. httpMethod=${httpMethod}`)
+  }
 })
 
 function middyfy (handler: Handler<Event, Result>): middy.Middy<Event, Result> {
@@ -42,4 +59,22 @@ function middyfy (handler: Handler<Event, Result>): middy.Middy<Event, Result> {
       }
     }))
     .use(errorHandler())
+}
+
+async function executeUserGetter (event: Event): Promise<Result> {
+  const { userId } = event.pathParameters
+  const db = new aws.DynamoDB.DocumentClient()
+  const userRepo = new UserRepository({
+    db,
+    tableName: 'users'
+  })
+  const executor = new userGetter.Executor({
+    userId,
+    userRepo
+  })
+  const result = await executor.execute()
+  return {
+    statusCode: StatusCodes.OK,
+    body: JSON.stringify(result)
+  }
 }
