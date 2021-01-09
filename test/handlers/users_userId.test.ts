1d36ea51deb79bee4422a369fcb979e7e41b861a
Munieru <20086673+munierujp@users.noreply.github.com>
2021-01-09 23:07:28 +0900

    remove controller

--- test/handlers/users_userId.test.ts
+++ test/handlers/users_userId.test.ts
@@ -1,96 +1,93 @@
-import mockContext from 'aws-lambda-mock-context'
 import { StatusCodes } from 'http-status-codes'
-import { Controller } from '../../src/controllers/users_userId'
 import { handler } from '../../src/handlers/users_userId'
-import * as createErrorResponseModule from '../../src/modules/createErrorResponse'
-import type { Result } from '../../src/controllers/users_userId'
+import { userGetter } from '../../src/usecases'
+import type { Result } from '../../src/handlers/users_userId'
 
 /* eslint-disable-next-line @typescript-eslint/no-var-requires */
 const LambdaTester = require('lambda-tester')
 
 describe('handler()', () => {
-  const executeSpy = jest.spyOn(Controller.prototype, 'execute')
-  const createErrorResponseSpy = jest.spyOn(createErrorResponseModule, 'createErrorResponse')
+  describe('if httpMethod is GET', () => {
+    const executeSpy = jest.spyOn(userGetter.Executor.prototype, 'execute')
 
-  const context = mockContext()
-  const event = {
-    pathParameters: {
-      userId: '12345678901234567890123456789012'
-    }
-  }
-
-  afterEach(() => {
-    executeSpy.mockReset()
-    createErrorResponseSpy.mockReset()
-  })
-
-  it('returns success response if error did not occur when executing controller', async () => {
-    const controllerResult: Result = {
-      statusCode: StatusCodes.OK,
-      body: 'test body'
-    }
-    executeSpy.mockResolvedValue(controllerResult)
-
-    await LambdaTester(handler)
-      .context(context)
-      .event(event)
-      .expectResult((result: Result) => {
-        expect(result).toBe(controllerResult)
-        expect(executeSpy).toBeCalledTimes(1)
-        expect(createErrorResponseSpy).not.toBeCalled()
-      })
-  })
-
-  it('returns error response if error occurred when executing controller', async () => {
-    const error = new Error('test error')
-    executeSpy.mockRejectedValue(error)
-    const resp: Result = {
-      statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-      body: 'test body'
-    }
-    createErrorResponseSpy.mockReturnValue(resp)
-
-    await LambdaTester(handler)
-      .context(context)
-      .event(event)
-      .expectResult((result: Result) => {
-        expect(result).toBe(resp)
-        expect(executeSpy).toBeCalledTimes(1)
-        expect(createErrorResponseSpy).toBeCalledTimes(1)
-        expect(createErrorResponseSpy).toBeCalledWith(error)
-      })
-  })
-
-  describe('if validation error occurred', () => {
-    const expectBadRequest = async (event: Record<string, any>): Promise<void> => {
-      const resp: Result = {
-        statusCode: StatusCodes.BAD_REQUEST,
-        body: 'test body'
+    const event = {
+      httpMethod: 'GET',
+      pathParameters: {
+        userId: '12345678901234567890123456789012'
       }
-      createErrorResponseSpy.mockReturnValue(resp)
+    }
+
+    afterEach(() => {
+      executeSpy.mockReset()
+    })
+
+    it(`returns ${StatusCodes.OK} response if error did not occur when executing userGetter`, async () => {
+      const result: userGetter.Result = {
+        id: 'test id',
+        name: 'test name'
+      }
+      executeSpy.mockResolvedValue(result)
 
       await LambdaTester(handler)
-        .context(context)
         .event(event)
-        .expectResult((result: Result) => {
-          expect(result).toBe(resp)
-          expect(executeSpy).not.toBeCalled()
-          expect(createErrorResponseSpy).toBeCalledTimes(1)
+        .expectResult(({
+          statusCode,
+          body
+        }: Result) => {
+          expect(statusCode).toBe(StatusCodes.OK)
+          expect(JSON.parse(body)).toEqual(result)
+          expect(executeSpy).toBeCalledTimes(1)
+        })
+    })
+
+    it(`returns ${StatusCodes.INTERNAL_SERVER_ERROR} response if error occurred when executing userGetter`, async () => {
+      const error = new Error('test error')
+      executeSpy.mockRejectedValue(error)
+
+      await LambdaTester(handler)
+        .event(event)
+        .expectResult(({
+          statusCode,
+          body
+        }: Result) => {
+          expect(statusCode).toBe(StatusCodes.INTERNAL_SERVER_ERROR)
+          expect(body).toBe(error.message)
+          expect(executeSpy).toBeCalledTimes(1)
+        })
+    })
+  })
+
+  it(`returns ${StatusCodes.NOT_IMPLEMENTED} response if httpMethod is invalid`, async () => {
+    await LambdaTester(handler)
+      .event({
+        httpMethod: 'invalid httpMethod'
+      })
+      .expectResult(({ statusCode }: Result) => {
+        expect(statusCode).toBe(StatusCodes.NOT_IMPLEMENTED)
+      })
+  })
+
+  describe(`returns ${StatusCodes.BAD_REQUEST} response if validation error occurred`, () => {
+    const expectBadRequest = async (event: Record<string, any>): Promise<void> => {
+      await LambdaTester(handler)
+        .event(event)
+        .expectResult(({ statusCode }: Result) => {
+          expect(statusCode).toBe(StatusCodes.BAD_REQUEST)
         })
     }
 
     // eslint-disable-next-line jest/expect-expect
-    it('returns error response if pathParameters.userId does not exist', async () => {
+    it(`returns ${StatusCodes.BAD_REQUEST} response if pathParameters.userId does not exist`, async () => {
       await expectBadRequest({
         pathParameters: {}
       })
     })
 
     // eslint-disable-next-line jest/expect-expect
-    it('returns error response if pathParameters.userId is invalid', async () => {
+    it(`returns ${StatusCodes.BAD_REQUEST} response if pathParameters.userId is invalid`, async () => {
       await expectBadRequest({
         pathParameters: {
-          userId: 'invalid userId'
+          userId: '1234567890123456789012345678901'
         }
       })
     })
