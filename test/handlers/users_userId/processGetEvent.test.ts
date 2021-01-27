9ec3f718640a2faf6a11240961a2b3013e398864
Munieru <20086673+munierujp@users.noreply.github.com>
2021-01-27 10:31:28 +0900

    add processGetEvent.test.ts

---
+++ test/handlers/users_userId/processGetEvent.test.ts
@@ -0,0 +1,42 @@
+import { StatusCodes } from 'http-status-codes'
+import { processGetEvent } from '../../../src/handlers/users_userId/processGetEvent'
+import { UserGetter } from '../../../src/usecases'
+import type { User } from '../../../src/domain/models'
+import type { Event } from '../../../src/handlers/users_userId/Event'
+
+describe('processGetEvent()', () => {
+  const getSpy = jest.spyOn(UserGetter.prototype, 'get')
+  const event = {
+    pathParameters: {
+      userId: 'test userId'
+    }
+  } as unknown as Event
+
+  afterEach(() => {
+    getSpy.mockReset()
+  })
+
+  it('throws error if it occurred', async () => {
+    const error = new Error('test error')
+    getSpy.mockRejectedValue(error)
+
+    await expect(processGetEvent(event)).rejects.toThrow(error)
+    expect(getSpy).toBeCalledTimes(1)
+    expect(getSpy).toBeCalledWith(event.pathParameters.userId)
+  })
+
+  it(`returns ${StatusCodes.OK} response if error did not occur`, async () => {
+    const user: User = {
+      id: 'test id',
+      name: 'test name'
+    }
+    getSpy.mockResolvedValue(user)
+
+    const result = await processGetEvent(event)
+
+    expect(result.statusCode).toBe(StatusCodes.OK)
+    expect(JSON.parse(result.body)).toEqual(user)
+    expect(getSpy).toBeCalledTimes(1)
+    expect(getSpy).toBeCalledWith(event.pathParameters.userId)
+  })
+})
