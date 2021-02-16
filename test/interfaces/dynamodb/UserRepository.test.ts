bd8a5b0e7aa887f860aa67b2380b883e153c4874
Munieru <20086673+munierujp@users.noreply.github.com>
2021-02-16 15:12:05 +0900

    update UserRepository.test.ts

--- test/interfaces/dynamodb/UserRepository.test.ts
+++ test/interfaces/dynamodb/UserRepository.test.ts
@@ -5,35 +5,59 @@ import {
 } from 'fp-ts/lib/Option'
 import type { Some } from 'fp-ts/lib/Option'
 import type { User } from '../../../src/domain/models'
+import * as UserModule from '../../../src/domain/models/User'
 import { UserRepository } from '../../../src/interfaces/dynamodb/UserRepository'
 
 describe('UserRepository', () => {
   describe('findById()', () => {
+    const isUserSpy = jest.spyOn(UserModule, 'isUser')
     const getPromiseMock = jest.fn()
     const getMock = jest.fn()
+
     const dynamodb = {
       get: getMock
     } as unknown as DynamoDB.DocumentClient
     const repo = new UserRepository(dynamodb)
+    const id = 'test id'
+    const item: User = {
+      id,
+      name: 'test name'
+    }
 
     beforeEach(() => {
+      getPromiseMock.mockResolvedValue({
+        Item: item
+      })
       getMock.mockReturnValue({
         promise: getPromiseMock
       })
     })
 
     afterEach(() => {
+      isUserSpy.mockReset()
       getPromiseMock.mockReset()
       getMock.mockReset()
     })
 
-    describe('if record is invalid as User', () => {
-      const id = 'test id'
-
+    describe('if record is valid as User', () => {
       beforeEach(() => {
-        getPromiseMock.mockResolvedValue({
-          Item: {}
-        })
+        isUserSpy.mockReturnValue(true)
+      })
+
+      it('returns Some<User>', async () => {
+        const optionalUser = await repo.findById(id)
+        expect(isSome(optionalUser)).toBeTruthy()
+        expect((optionalUser as Some<User>).value).toBe(item)
+        expect(getMock).toBeCalledTimes(1)
+        expect(getPromiseMock).toBeCalledTimes(1)
+        expect(isUserSpy).toBeCalledTimes(1)
+        expect(isUserSpy).toBeCalledWith(item)
+      })
+    })
+
+    describe('if record is invalid as User', () => {
+      beforeEach(() => {
+        isUserSpy.mockReturnValue(false)
       })
 
       it('returns None', async () => {
@@ -41,28 +65,8 @@ describe('UserRepository', () => {
         expect(isNone(optionalUser)).toBeTruthy()
         expect(getMock).toBeCalledTimes(1)
         expect(getPromiseMock).toBeCalledTimes(1)
-      })
-    })
-
-    describe('if record is valid as User', () => {
-      const id = 'test id'
-      const user = {
-        id,
-        name: 'test name'
-      }
-
-      beforeEach(() => {
-        getPromiseMock.mockResolvedValue({
-          Item: user
-        })
-      })
-
-      it('returns Some<User>', async () => {
-        const optionalUser = await repo.findById(id)
-        expect(isSome(optionalUser)).toBeTruthy()
-        expect((optionalUser as Some<User>).value).toBe(user)
-        expect(getMock).toBeCalledTimes(1)
-        expect(getPromiseMock).toBeCalledTimes(1)
+        expect(isUserSpy).toBeCalledTimes(1)
+        expect(isUserSpy).toBeCalledWith(item)
       })
     })
   })
